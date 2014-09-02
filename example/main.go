863e5fc29b9b548d3168275c56a83f6ce132b0f1
Vincenzo Maggio <v.maggio82@gmail.com>
2014-09-03 01:01:54 +0200

    Revised split() function: - Now navigating left rope use the new left rope as node rope; - Second rope is created by Concat() going up through the rope.

--- example/main.go
+++ example/main.go
@@ -5,19 +5,20 @@ import (
 	"github.com/vinzmay/go-rope"
 )
 
-func main() {
+func main3() {
 	a := rope.New("Hello, 世界")
 	fmt.Println("a:", a)
 	fmt.Println("a len:", a.Len())
-	//fmt.Println("a json:", a.ToJSON())
 	a1, a2 := a.Split(7)
-	fmt.Println("a1:", a1)
-	fmt.Println("a2:", a2)
+	fmt.Println("a1:", a1.ToJSON())
+	fmt.Println("a2:", a2.ToJSON())
 	fmt.Println("a_test:", a1.Concat(a2))
 	fmt.Println("a[8]:", string(a.Index(8)))
+	fmt.Println("a[8-9]:", string(a.Report(8, 2)))
+	main2()
 }
 
-func main2() {
+func main() {
 	e := rope.New("Hello_")
 	f := rope.New("my_")
 	c := e.Concat(f)
@@ -30,17 +31,18 @@ func main2() {
 	d := g.Concat(h)
 	b := c.Concat(d)
 	a := b.Concat(nil)
-	a1, a2 := a.Split(7)
-	a3 := a.Insert(9, "abcde")
-	a4 := a.Delete(1, 2)
-	fmt.Println("a", a)
-	//fmt.Println(string(a.ToJSON()))
+	a1, a2 := a.Split(2)
+	//fmt.Println("a", a.ToJSON())
 	fmt.Println("a1", a1)
-	//fmt.Println(string(a1.ToJSON()))
+	fmt.Println("a1j", a1.ToJSON())
 	fmt.Println("a2", a2)
-	//fmt.Println(string(a2.ToJSON()))
-	fmt.Println("a3", a3)
-	fmt.Println(string(a3.ToJSON()))
-	fmt.Println("a4", a4.ToJSON())
-	fmt.Println(a4.Report(1, 20))
+	fmt.Println("a2", a2.ToJSON())
+}
+
+func main2() {
+	a := rope.New("abc")
+	a1, a2 := a.Split(1)
+	fmt.Println(a)
+	fmt.Println(a1)
+	fmt.Println(a2)
 }
--- rope.go
+++ rope.go
@@ -8,6 +8,7 @@ package rope
 import (
 	"bytes"
 	"encoding/json"
+	_ "fmt"
 	"unicode/utf8"
 )
 
@@ -15,6 +16,7 @@ import (
 type Rope struct {
 	value  []rune
 	weight int
+	length int
 	left   *Rope
 	right  *Rope
 }
@@ -25,7 +27,10 @@ func (rope *Rope) isLeaf() bool {
 
 //New returns a new rope initialized with given string
 func New(bootstrap string) *Rope {
-	return &Rope{value: []rune(bootstrap), weight: utf8.RuneCountInString(bootstrap)}
+	return &Rope{
+		value:  []rune(bootstrap),
+		weight: utf8.RuneCountInString(bootstrap),
+		length: len([]rune(bootstrap))}
 }
 
 //Len returns the length of the rope underlying string
@@ -33,7 +38,7 @@ func (rope *Rope) Len() int {
 	if rope == nil {
 		return 0
 	}
-	return rope.weight + rope.right.Len()
+	return rope.length
 }
 
 //String returns the complete string stored in the rope
@@ -51,8 +56,9 @@ func (rope *Rope) String() string {
 
 //Internal struct for generating JSON
 type ropeForJSON struct {
-	Value  []rune
+	Value  string
 	Weight int
+	Length int
 	Left   *ropeForJSON
 	Right  *ropeForJSON
 }
@@ -64,7 +70,8 @@ func (rope *Rope) toRopeForJSON() *ropeForJSON {
 	}
 	return &ropeForJSON{
 		Weight: rope.weight,
-		Value:  rope.value,
+		Value:  string(rope.value),
+		Length: rope.length,
 		Left:   rope.left.toRopeForJSON(),
 		Right:  rope.right.toRopeForJSON(),
 	}
@@ -92,14 +99,19 @@ func (rope *Rope) Index(idx int) rune {
 
 //Concat merges two ropes and generates a brand new one
 func (rope *Rope) Concat(other *Rope) *Rope {
+	//Special case: if the first rope is nil, just return the second rope
+	if rope == nil {
+		return other
+	}
 	//Special case: if the other rope is nil, just return the first rope
 	if other == nil {
 		return rope
 	}
-	//Return a new rope with left and right subropes assigned respectively
-	//to 'rope' and 'other'. Weight is the len of left rope.
+	//Return a new rope with 'rope' and 'other' assigned respectively
+	//to left and right subropes. Weight is the len of left rope.
 	return &Rope{
 		weight: rope.Len(),
+		length: rope.length + other.length,
 		left:   rope,
 		right:  other,
 	}
@@ -109,74 +121,51 @@ func (rope *Rope) Concat(other *Rope) *Rope {
 //It accepts idx to split (1-based), a slice for the rope parts
 //to be used for the second rope, a slice for the rope whose weight
 //must be updated, and a slice to record weights to remove
-func (rope *Rope) internalSplit(idx int,
-	snd []*Rope,
-	upd []*Rope,
-	updCount []int) (*Rope, []*Rope, []*Rope, []int) {
+func (rope *Rope) split(idx int,
+	secondRope *Rope) (*Rope, *Rope) {
 	//If idx is equal to rope weight, we're arrived:
-	//- Update upd slice with the first weight to remove from parents
-	//- Add right rope as starter for the second rope slice;
-	//- Create a rope equal to the original but without right rope
+	//- If is leaf, return it;
+	//- Otherwise, return its left rope.
+	//Right rope initialises secondRope.
 	if idx == rope.weight {
-		updCount = append(updCount, rope.right.Len())
-		return &Rope{
-			weight: rope.weight,
-			value:  rope.value,
-			left:   rope.left,
-		}, append(snd, rope.right), upd, updCount
+		var r *Rope
+		if rope.isLeaf() {
+			r = rope
+		} else {
+			r = rope.left
+		}
+		return r, rope.right
 	} else if idx > rope.weight {
 		//We have to go right, call the function on right side with appropriate index.
 		//Builds the rope and pass it up the stack with the other parameters.
-		newRight, snd, upd, updCount := rope.right.internalSplit(idx-rope.weight, snd, upd, updCount)
+		newRight, secondRope := rope.right.split(idx-rope.weight, secondRope)
 		return &Rope{
 			weight: rope.weight,
-			value:  rope.value,
 			left:   rope.left,
 			right:  newRight,
-		}, snd, upd, updCount
-	} else if idx < rope.weight && !rope.isLeaf() {
-		newLeft, snd, upd, updCount := rope.left.internalSplit(idx, snd, upd, updCount)
-		snd = append(snd, rope.right)
-		fst := &Rope{
-			weight: rope.weight,
-			left:   newLeft,
-		}
-		upd = append(upd, fst)
-		updCount = append(updCount, rope.right.Len())
-		return fst, snd, upd, updCount
-	} else {
-		sndW := len(rope.value) - idx
-		updCount := append(updCount, sndW)
-		snd = append(snd, &Rope{weight: sndW, value: rope.value[idx:len(rope.value)]})
+		}, secondRope
+	} else if rope.isLeaf() { //idx < rope.weight, go left!
+		//It's a leaf: we have to create a new rope by splitting leaf at index
 		return &Rope{
-			weight: idx,
-			value:  rope.value[0:idx],
-		}, snd, upd, updCount
+				weight: idx,
+				value:  rope.value[0:idx],
+				length: idx,
+			}, secondRope.Concat(&Rope{
+				weight: len(rope.value) - idx,
+				value:  rope.value[idx:len(rope.value)],
+				length: len(rope.value) - idx,
+			})
+	} else {
+		newLeft, secondRope := rope.left.split(idx, secondRope)
+		return newLeft, secondRope.Concat(rope.right)
 	}
 }
 
 //Split generates two strings starting from one,
 //splitting it at input index (1-based)
 func (rope *Rope) Split(idx int) (firstRope *Rope, secondRope *Rope) {
-	snd := make([]*Rope, 0)
-	upd := make([]*Rope, 0)
-	updCount := make([]int, 0)
-	firstRope, snd, upd, updCount = rope.internalSplit(idx, snd, upd, updCount)
-	for i, r := range snd {
-		if r != nil {
-			if i == 0 {
-				secondRope = r
-			} else {
-				secondRope = secondRope.Concat(r)
-			}
-		}
-	}
-	var w int
-	for i, u := range upd {
-		w += updCount[i]
-		u.weight = u.weight - w
-	}
-	return firstRope, secondRope
+	//Create the slices for split
+	return rope.split(idx, secondRope)
 }
 
 //Insert generates a new rope inserting a string into the
@@ -197,26 +186,30 @@ func (rope *Rope) Delete(idx int, len int) *Rope {
 }
 
 //Report return a substring of the rope starting from idx included (1-based)
-//for len bytes
-func (rope *Rope) Report(idx int, lenght int) []rune {
+//for len runes
+func (rope *Rope) Report(idx int, length int) string {
+	return string(rope.internalReport(idx, length))
+}
+
+func (rope *Rope) internalReport(idx int, length int) []rune {
 	//if idx > rope.weight we go right with modified idx
 	if idx > rope.weight {
-		return rope.right.Report(idx-rope.weight, lenght)
+		return rope.right.internalReport(idx-rope.weight, length)
 	} else
 	//if idx <= rope.weight we check if the left branch
 	//has enough values to fetch report, else we split
-	if rope.weight >= idx+lenght-1 {
+	if rope.weight >= idx+length-1 {
 		//we have enough space, just go left (if there is a left!)
 		if !rope.isLeaf() {
-			return rope.left.Report(idx, lenght)
+			return rope.left.internalReport(idx, length)
 		} else {
 			//we're in a leaf, fetch from here
-			return rope.value[idx-1 : idx+lenght-1]
+			return rope.value[idx-1 : idx+length-1]
 		}
 	} else {
 		//Split the work and then merge both parts
-		l := rope.left.Report(idx, rope.weight-idx+1)
-		r := rope.right.Report(1, lenght-rope.weight+idx-1)
+		l := rope.left.internalReport(idx, rope.weight-idx+1)
+		r := rope.right.internalReport(1, length-rope.weight+idx-1)
 		s := make([]rune, len(l)+len(r))
 		for i := 0; i < len(l); i++ {
 			s[i] = l[i]
