2c5b9ea9b11c97caa0c653d5a70e76bcb4ccf448
Vincenzo Maggio <v.maggio82@gmail.com>
2014-09-03 17:51:12 +0200

    Added doc.go

---
+++ doc.go
@@ -0,0 +1,11 @@
+/*
+Package go-rope provides a persistent rope data structure for storing large texts.
+
+Refer to README.md for further information.
+
+Installation
+
+	go get github.com/vinzmay/go-rope
+
+*/
+package rope
